f1736155a25065017d54c482effcd45680380461
pranava s balugari <stalin.pranava@gmail.com>
2020-09-24 14:59:23 +1000

    Gateway file to the project where all the files are required

--- lib/family/tree.rb
+++ lib/family/tree.rb
@@ -1,8 +1,21 @@
 require "family/tree/version"
-
+require "family/tree/person"
+require "family/tree/cohort"
+require "family/tree/builder"
+require "family/tree/relations"
+require "family/tree/executor"
+require "family/tree/command"
+require "family/tree/cli"
 module Family
   module Tree
+    FILE_NOT_SUPPORTED_ERROR = "Only txt files are supported currently.".freeze
+    ACCEPTED_FORMATS = [".txt"]
+
     class Error < StandardError; end
-    # Your code goes here...
+    class FileFormatNotSupportedError < StandardError
+        def initialize
+          super(FILE_NOT_SUPPORTED_ERROR)
+        end
+    end
   end
 end
